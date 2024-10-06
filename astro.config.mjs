f59dce2229b0940d71e45078f1f3671af83cc702
Jonathon Ho <jonathonmho@gmail.com>
2024-10-07 00:01:08 +0900

    Add GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://afkjon.github.com',
+	base: 'blog',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
