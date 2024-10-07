756eb1a52766c593093c8c398da217d57d993105
Jonathon Ho <jonathonmho@gmail.com>
2024-10-07 10:21:33 +0900

    Change base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://afkjon.github.com',
-	base: '/',
+	base: '/blog/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
